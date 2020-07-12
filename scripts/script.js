145e06a46b846f2fad9ddb67132df0a6948147c8
rvsverchkov <s10z40@yandex.ru>
2020-07-12 13:33:07 +0300

    validation - 2/4

--- scripts/script.js
+++ scripts/script.js
@@ -17,8 +17,8 @@ const placeInput = document.querySelector('.popup__input_type_place');
 const linkInput = document.querySelector('.popup__input_type_link');
 const cardTemplate = document.querySelector('#card').content;
 const elements = document.querySelector('.elements');
-const buttonLike = cardTemplate.querySelector('.card__like');
 const previewCloseButton = document.querySelector('.popup__close-preview');
+const saveButtons = document.querySelectorAll('.popup__save');
 
 const popupEditProfile = function() {
     popupEdit.classList.toggle('popup_opened');
@@ -78,6 +78,56 @@ const createCard = function(name, link) {
     elements.prepend(card);
 }
 
+const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+        saveButtons.forEach(function(saveButton) {
+            saveButton.classList.add('popup__save_type_disabled');
+        });
+        formElementEdit.removeEventListener('submit', formSubmitHandlerProfile);
+        formElementAdd.removeEventListener('submit', formSubmitHandlerCard);
+    } else {
+        hideInputError(formElement, inputElement);
+        saveButtons.forEach(function(saveButton) {
+            saveButton.classList.remove('popup__save_type_disabled');
+        });
+        formElementEdit.addEventListener('submit', formSubmitHandlerProfile);
+        formElementAdd.addEventListener('submit', formSubmitHandlerCard);
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', function() {
+            checkInputValidity(formElement, inputElement);
+        });
+    });
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorItem = formElement.querySelector(`#${inputElement.id}-error`);
+    errorItem.textContent = errorMessage;
+    inputElement.classList.add('popup__input_type_error');
+
+};
+
+const hideInputError = (formElement, inputElement) => {
+    const errorItem = formElement.querySelector(`#${inputElement.id}-error`);
+    errorItem.textContent = '';
+    inputElement.classList.remove('popup__input_type_error');
+}
+
+const activateValidation = () => {
+    const formList = Array.from(document.querySelectorAll('.popup__form'))
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement);
+    });
+};
+
 formElementEdit.addEventListener('submit', formSubmitHandlerProfile);      //Переместил слушатели событий в конец файла
 popupOpenButton.addEventListener('click', popupEditProfile);
 popupCloseEditButton.addEventListener('click', popupEditProfile);
@@ -89,3 +139,5 @@ previewCloseButton.addEventListener('click', previewCard);
 initialCards.forEach(function(element) {
     createCard(element.name, element.link);
 });
+
+activateValidation();
